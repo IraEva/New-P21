4b34319df0729fb83365c3277e2b2ae29dc7fafc
IraEva <86668079+IraEva@users.noreply.github.com>
2021-11-04 16:32:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball; 
+
+var leftSide;
+var rightSide; 
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+ leftSide= new Ground(20,200,20,40);
+ rightSide= new Ground(390,200,20,400);
+
+	var ball_options= {
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
+	}
+      
+	//Create the Bodies Here.
+    ball=Bodies.circle(200,100,20,ball_options);
+  World.add(world, ball);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  }
+
+
+function draw() {
+   background(0);
+  leftSide.show()
+  rightSide.show()
+  ellipse(ball.position.x, ball.position.y, 10)
+
+  Ground.show()
+  left.show();
+  right.show();
+  Engine.update(engine);
+ //apply force
+  function hForce(){
+	Matter.Body.applyForce(ball,{x:0, y:0},{x:0.05, y:0})
+  }
+  
+  function vForce(){
+	Matter.Body.applyForce(ball,{x:0, y:0},{x:0, y:-0.05})
+  }
+  
+  //make the ball move
+  function KeyPressed (){
+ if(keyCode == UP_ARROW){
+	 //apply force to ball
+	 ball.mouseClicked(hForce);
+	 ball.mouseClicked(vForce);
+ }
+  }
+   drawSprites();
+ 
+}
+
+
+
